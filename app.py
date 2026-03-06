45d3a0c1b79d4cecc6019504407981827254de3f
Денис Крапп <deniskrappik@gmail.com>
2026-03-06 11:25:11 +0800

    new branch first commit

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ def login(username):
     print(f"User {username} logged in. Hello!")
     print(f"Welcpme, {username}")
 
+def logout(username):
+    print(f"User {username} logged out.")
+
 if __name__ == "__main__":
     main()
