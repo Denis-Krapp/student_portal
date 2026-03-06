3553fd15bb11dcb0ddbafabd9a3ee48eb1dcb9a4
Денис Крапп <deniskrappik@gmail.com>
2026-03-06 11:23:02 +0800

    second commit

--- app.py
+++ app.py
@@ -4,6 +4,9 @@
 def main():
     print("Student Portal System")
 
+def login(username):
+    print(f"User {username} logged in. Hello!")
+    print(f"Welcpme, {username}")
 
 if __name__ == "__main__":
     main()
